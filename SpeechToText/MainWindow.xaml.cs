790ffa63fd4aab34cdb7197b0f99d35acf155990
FlyingZ <fl05151n@hotmail.com>
2016-03-05 18:13:33 +0200

    V normal

---
+++ SpeechToText/MainWindow.xaml.cs
@@ -0,0 +1,267 @@
+﻿#region using directives
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Windows;
+using System.Diagnostics;
+using System.Text;
+using System.IO.Ports;
+using System.ComponentModel;
+using System.Data;
+using System.Speech.Synthesis;
+#endregion
+
+
+
+namespace SpeechToText
+{
+    /// <summary>
+    /// MainWindow class
+    /// </summary>
+  
+    public partial class MainWindow : Window
+    {
+        #region locals
+
+        /// <summary>
+        /// the engine
+        /// </summary>
+        SpeechRecognitionEngine speechRecognitionEngine = null;
+
+        /// <summary>
+        /// list of predefined commands
+        /// </summary>
+        List<Word> words = new List<Word>();
+
+        #endregion
+
+        #region ctor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                // create the engine
+                speechRecognitionEngine = createSpeechEngine("en-US");
+
+                // hook to events
+                speechRecognitionEngine.AudioLevelUpdated += new EventHandler<AudioLevelUpdatedEventArgs>(engine_AudioLevelUpdated);
+                speechRecognitionEngine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(engine_SpeechRecognized);
+
+                // load dictionary
+                loadGrammarAndCommands();
+
+                // use the system's default microphone
+                speechRecognitionEngine.SetInputToDefaultAudioDevice();
+
+                // start listening
+                speechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Voice recognition failed");
+            }
+        }
+
+        #endregion
+
+        #region internal functions and methods
+
+        /// <summary>
+        /// Creates the speech engine.
+        /// </summary>
+        /// <param name="preferredCulture">The preferred culture.</param>
+        /// <returns></returns>
+        private SpeechRecognitionEngine createSpeechEngine(string preferredCulture)
+        {
+            foreach (RecognizerInfo config in SpeechRecognitionEngine.InstalledRecognizers())
+            {
+                if (config.Culture.ToString() == preferredCulture)
+                {
+                    speechRecognitionEngine = new SpeechRecognitionEngine(config);
+                    break;
+                }
+            }
+
+            // if the desired culture is not found, then load default
+            if (speechRecognitionEngine == null)
+            {
+                MessageBox.Show("The desired culture is not installed on this machine, the speech-engine will continue using "
+                    + SpeechRecognitionEngine.InstalledRecognizers()[0].Culture.ToString() + " as the default culture.",
+                    "Culture " + preferredCulture + " not found!");
+                speechRecognitionEngine = new SpeechRecognitionEngine(SpeechRecognitionEngine.InstalledRecognizers()[0]);
+            }
+
+            return speechRecognitionEngine;
+        }
+
+        /// <summary>
+        /// Loads the grammar and commands.
+        /// </summary>
+        private void loadGrammarAndCommands()
+        {
+            try
+            {
+                Choices texts = new Choices();
+                string[] lines = File.ReadAllLines(Environment.CurrentDirectory + "\\example.txt");
+                foreach (string line in lines)
+                {
+                    // skip commentblocks and empty lines..
+                    if (line.StartsWith("--") || line == String.Empty) continue;
+
+                    // split the line
+                    var parts = line.Split(new char[] { '|' });
+
+                    // add commandItem to the list for later lookup or execution
+                    words.Add(new Word() { Text = parts[0], AttachedText = parts[1], IsShellCommand = (parts[2] == "true") });
+
+                    // add the text to the known choices of speechengine
+                    texts.Add(parts[0]);
+                    texts.Add("saaaa");
+                }
+                Grammar wordsList = new Grammar(new GrammarBuilder(texts));
+                speechRecognitionEngine.LoadGrammar(wordsList);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Gets the known command.
+        /// </summary>
+        /// <param name="command">The order.</param>
+        /// <returns></returns>
+        private string getKnownTextOrExecute(string command)
+        {
+            try
+            {
+                var cmd = words.Where(c => c.Text == command).First();
+
+                if (cmd.IsShellCommand)
+                {
+                    Process proc = new Process();
+                    proc.EnableRaisingEvents = false;
+                    proc.StartInfo.FileName = cmd.AttachedText;
+                    proc.Start();
+                    return "you just started : " + cmd.AttachedText;
+                }
+                else
+                {
+                    SerialPort port;
+                    SpeechSynthesizer reader = new SpeechSynthesizer();
+                    String s = cmd.AttachedText;
+
+                        port = new SerialPort("COM5", 9600);
+                        port.Open();       
+                  
+                    if (s.Equals("go"))
+                    {
+                        reader.SpeakAsync("Engine started");
+                        port.Write("w" + '\n');
+                        
+                    }
+                    if (s.Equals("back"))
+                    {
+                        port.Write("s" + '\n');
+                    }
+                    if (s.Equals("left"))
+                    {
+                        port.Write("a" + '\n');
+                    }
+                    if (s.Equals("right"))
+                    {
+                        port.Write("d" + '\n');
+                    }
+                    if (s.Equals("stop"))
+                    {
+                        port.Write("o" + '\n');
+                        reader.SpeakAsync("Engine Stopped");
+                    }
+                    if(s.Equals("half right"))
+                    {
+                        port.Write("e" + "\n");
+                    }
+                    if(s.Equals("half left"))
+                    {
+                        port.Write("q" + "\n");
+                    }
+                    if (s.Equals("Iva do you love me"))
+                    {
+                        reader.SpeakAsync(cmd.AttachedText);
+                    }
+                    
+                port.Close();
+
+                    return cmd.AttachedText;
+                }
+            }
+            catch (Exception)
+            {
+                return command;
+            }
+        }
+
+        #endregion
+
+        #region speechEngine events
+
+        /// <summary>
+        /// Handles the SpeechRecognized event of the engine control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Speech.Recognition.SpeechRecognizedEventArgs"/> instance containing the event data.</param>
+        void engine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            txtSpoken.Text += "\r" + getKnownTextOrExecute(e.Result.Text);
+            scvText.ScrollToEnd();
+        }
+
+        /// <summary>
+        /// Handles the AudioLevelUpdated event of the engine control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Speech.Recognition.AudioLevelUpdatedEventArgs"/> instance containing the event data.</param>
+        void engine_AudioLevelUpdated(object sender, AudioLevelUpdatedEventArgs e)
+        {
+            prgLevel.Value = e.AudioLevel;
+        }
+
+        #endregion
+
+        #region window closing
+
+        /// <summary>
+        /// Handles the Closing event of the Window control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.ComponentModel.CancelEventArgs"/> instance containing the event data.</param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // unhook events
+            speechRecognitionEngine.RecognizeAsyncStop();
+            // clean references
+            speechRecognitionEngine.Dispose();
+        }
+
+        #endregion
+
+        #region GUI events
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+    }
+}
